aeb5b1f58665c59d1053b810a642e6f3efa3ee08
zcc <1070548197@qq.com>
2023-08-14 14:34:05 +0800

    修改组件名称

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 import echarts from '../components/index.vue'
 
 const echart={
-    install(app,options){
-        app.component('echarts',echarts);
+    install(app){
+        app.component('echart',echarts);
     }
 }
 export default echart;
